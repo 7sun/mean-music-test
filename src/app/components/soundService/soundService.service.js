cd2d6083807a3947e0511547a5de7c487a489e05
7sun <pakrouse@gmail.com>
2015-11-05 15:52:48 -0800

    sets up initial project

---
+++ src/app/components/soundService/soundService.service.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular
+      .module('meanMusic')
+      .service('soundService', soundService);
+
+  function soundService($http, $q) {
+    var apiKeyParam = '?client_id=bda4ada8694db06efcac9cf97b872b3e';
+
+    this.getSounds = getSounds;
+    this.searchArtists = searchArtists;
+
+    function searchArtists(artist){
+      var deferred = $q.defer();
+      $http.get('http://api.soundcloud.com/users' + apiKeyParam + '&q=' + artist)
+      .success(function(data){
+        deferred.resolve(data);
+      })
+      .error(function(errors){
+        console.log("search errors: ", errors);
+      })
+      return deferred.promise
+    }
+
+    function getSounds(artist) {
+      if (!artist){
+        artist = 'flume';
+      }
+      var deferred = $q.defer();
+      $http.get('http://api.soundcloud.com/users/' + artist + '/tracks.json' + apiKeyParam)
+        .success(function(data){
+          deferred.resolve(data);
+        })
+        .error(function(errors){
+          console.log("getSounds errors: ", errors);
+        })
+      return deferred.promise;
+    }
+  }
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('meanMusic')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, soundService) {
+    var vm = this;
+    var apiKeyParam = '?client_id=bda4ada8694db06efcac9cf97b872b3e';
+
+    vm.sounds = [];
+    vm.artist;
+    vm.currentTrack;
+    vm.getSounds = getSounds;
+    vm.searchArtists = searchArtists;
+    vm.submitArtist = submitArtist;
+    vm.loadTrack = loadTrack;
+
+    getSounds();
+
+    function searchArtists(artist){
+      soundService.searchArtists(artist)
+      .then(function(data){
+        vm.artists = data;
+      })
+    }
+
+    function getSounds(artist) {
+      console.log("artist: ", artist);
+      soundService.getSounds(artist)
+      .then(function(data){
+        console.log("getsounds data: ", data);
+        vm.sounds = data;
+      })
+    }
+
+    function submitArtist(){
+      searchArtists(vm.artist);
+    }
+
+    function loadTrack(sound){
+      sound.streamable ? vm.currentTrack = sound.stream_url + apiKeyParam: alert("This song is not available for streaming");
+    }
+  }
+})();
